34416355bffd3d8c9e1297c3261e36867a6d9902
AKHIL MITTAL <mittalakhil.1992@gmail.com>
2019-11-26 17:44:20 +0530

    added deployment error

--- src/main/java/org/codejudge/sb/Application.java
+++ src/main/java/org/codejudge/sb/Application.java
@@ -1,18 +1,19 @@
 package org.codejudge.sb;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
 @ComponentScan({"org.codejudge.sb"})
 @Slf4j
 public class Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		log.info("Starting Application...");
-		SpringApplication.run(Application.class, args);
+		throw new Exception("Abcd");
+//		SpringApplication.run(Application.class, args);
 	}
 
 }
